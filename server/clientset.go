4992b42e866bfad9787e7954ac435d70af74d5e5
hashfunc <root@hashfunc.io>
2022-05-23 22:45:14 +0900

    feat: Add clientset functions to server

---
+++ server/clientset.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func getClientConfigLoader() clientcmd.ClientConfig {
+	rules := clientcmd.NewDefaultClientConfigLoadingRules()
+	rules.DefaultClientConfig = &clientcmd.DefaultClientConfig
+	overrides := clientcmd.ConfigOverrides{}
+	return clientcmd.NewInteractiveDeferredLoadingClientConfig(rules, &overrides, os.Stdin)
+}
+
+func GetClientset() (*kubernetes.Clientset, error) {
+	loader := getClientConfigLoader()
+
+	config, err := loader.ClientConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return clientset, nil
+}
